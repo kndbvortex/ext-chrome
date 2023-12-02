1ac4eaf332b940af92b67be6b6dc46dd0cde403f
Durande Kamga <kamganguifo@gmail.com>
2023-12-02 23:07:27 +0100

    redirect to freedium extension

---
+++ fredium-next-tab/content.js
@@ -0,0 +1,10 @@
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action === "redirect") {
+    let url = window.location.href;
+    let f_url = "https://freedium.cfd/";
+    sendResponse({ message: "Redirecting ...." });
+    if (!url.includes(f_url)) {
+      window.location.replace(f_url + url);
+    }
+  }
+});
