b5781e5cef37383db81a72f7a04ddb99658b5737
Durande Kamga <kamganguifo@gmail.com>
2024-04-09 18:00:05 +0200

    consider history

--- fredium-next-tab/content.js
+++ fredium-next-tab/content.js
@@ -4,7 +4,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     let f_url = "https://freedium.cfd/";
     sendResponse({ message: "Redirecting ...." });
     if (!url.includes(f_url)) {
-      window.location.replace(f_url + url);
+      window.location = f_url + url;
     }
   }
 });
